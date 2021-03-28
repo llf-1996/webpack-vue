d19cd7c6bd5a869a87f9ea96cbdeb554d786aecf
llf <2367746876@qq.com>
2021-03-28 23:28:24 +0800

    首页九宫格样式

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -6,7 +6,45 @@
         <img :src="item.img" alt="">
       </mt-swipe-item>
     </mt-swipe>
-    <h3>HomeContainer</h3>
+    <!-- 九宫格 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu1.png" alt="新闻资讯">
+          <div class="mui-media-body">新闻资讯</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu2.png" alt="图片分享">
+          <div class="mui-media-body">图片分享</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu3.png" alt="商品购买">
+          <div class="mui-media-body">商品购买</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu4.png" alt="留言反馈">
+          <div class="mui-media-body">留言反馈</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu5.png" alt="视频专区">
+          <div class="mui-media-body">视频专区</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu6.png" alt="联系我们">
+          <div class="mui-media-body">联系我们</div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -69,6 +107,20 @@
         width: 100%;
         height: 100%;
       }
+    }
+  }
+  .mui-grid-view.mui-grid-9 {
+    background-color: #fff;
+    border: none;
+  }
+  .mui-grid-view.mui-grid-9 .mui-table-view-cell {
+    border: 0;
+    img {
+      width: 60px;
+      height: 60px;
+    }
+    .mui-media-body {
+      font-size: 13px;
     }
   }
 </style>
